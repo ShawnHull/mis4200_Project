7e538b05abc81545116e2e6a19f3f7f4ea5f4648
Zack Margelowsky <zm064016@ohio.edu>
2021-03-16 10:24:44 -0400

    employee information for profile

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -7,5 +7,19 @@ namespace mis4200_Project.Models
 {
     public class Profile
     {
+        public int profileID { get; set; }
+        public string employeeFirstName { get; set; }
+        public string employeeLastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime employeeSince { get; set; }
+        public string employeeFullName
+        {
+            get
+            {
+                return employeeLastName + ", " + employeeFirstName;
+            }
+
+        }
     }
 }
