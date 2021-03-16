439fe5a6201e65b43698af310a2a972ebe020a0b
Zack Margelowsky <zm064016@ohio.edu>
2021-03-16 11:15:09 -0400

    entered context into CContext.cs

--- DAL/CContext.cs
+++ DAL/CContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using mis4200_Project.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace mis4200_Project.DAL
 {
-    public class CContext
+    public class CContext : DbContext 
     {
+        public CContext() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Profile> profile { get; set; }
     }
 }
