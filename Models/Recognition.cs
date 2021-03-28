fbdfe7f62379958f8c979217852102660503ef8d
Zack Margelowsky <zm064016@ohio.edu>
2021-03-28 19:15:28 -0400

    Recognition Fields

--- Models/Recognition.cs
+++ Models/Recognition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,26 @@ namespace mis4200_Project.Models
 {
     public class Recognition
     {
-    }
+        public int RecognitionID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string employeeFirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string employeeLastName { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Display(Name = "Phone")]
+        public string phone { get; set; }
+        [Display(Name = "Date of Recognition")]
+        public DateTime recognitionDate { get; set; }
+        [Display(Name = "Centric Unit")]
+        public string Unit { get; set; }
+        [Display(Name = "Full Name")]
+        public string employeeFullName
+        {
+            get
+            {
+                return employeeLastName + ", " + employeeFirstName;
+            }
+        }
 }
