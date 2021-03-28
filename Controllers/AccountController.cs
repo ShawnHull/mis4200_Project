8d88ff198fb2dd60b97e89381d9bd30c9573dc0e
Kyle Umek <ku502616@ohio.edu>
2021-03-28 18:51:14 -0400

    Updated registration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace mis4200_Project.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Create", "Profiles");
                 }
                 AddErrors(result);
             }
